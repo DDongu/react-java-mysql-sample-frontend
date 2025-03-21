81514a23dfa31eb4a1ad308b74c9ccbb72bc3bac
김동규 <dongu@MacBook-Pro.local>
2025-03-21 13:07:48 +0900

    Edit: 도커허브 username 설정

--- deployment/scripts/start_new_containers.sh
+++ deployment/scripts/start_new_containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 최신 Docker 이미지 가져오기
-docker pull my-docker-hub-username/my-react-app:latest
+docker pull ddongu/my-react-app:latest
 
 # 새로운 컨테이너 2개 실행 (포트 3000, 3001)
-docker run -d -p 3000:3000 --name my-react-app-1 my-docker-hub-username/my-react-app:latest
+docker run -d -p 3000:3000 --name my-react-app-1 ddongu/my-react-app:latest
 # docker run -d -p 3001:3000 --name my-react-app-2 my-docker-hub-username/my-react-app:latest
