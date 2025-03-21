09dcf949dcb305c95c8532bb7398304bcf97f679
김동규 <dongu@MacBook-Pro.local>
2025-03-21 10:51:47 +0900

    Edit: CD 부분에 임시컨테이너 삭제 스크립트 추가

--- deployment/scripts/stop_old_containers.shstop_old_containers.sh
+++ deployment/scripts/stop_old_containers.shstop_old_containers.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "기존 컨테이너 확인 및 정리"
+# 임시 컨테이너(terraform 시작 템플릿) 중지 및 삭제
+if [ "$(docker ps -q -f name=temp-nginx)" ]; then
+    echo "임시 컨테이너(temp-nginx) 삭제"
+    docker stop temp-nginx && docker rm temp-nginx
+fi
+
 # 기존 컨테이너 중지 및 삭제
 docker stop my-react-app-1 || true
 # docker stop my-react-app-2 || true
