87f323bdcab170f904169bdf3543daf946319326
김동규 <dongu@MacBook-Pro.local>
2025-03-25 13:58:15 +0900

    Edit: 컨테니어 이름 변경

--- deployment/scripts/stop_old_containers.sh
+++ deployment/scripts/stop_old_containers.sh
@@ -7,8 +7,8 @@ if [ "$(docker ps -q -f name=temp-nginx)" ]; then
 fi
 
 # 기존 컨테이너 중지 및 삭제
-docker stop my-react-app-1 || true
+docker stop my-react-app || true
 # docker stop my-react-app-2 || true
-docker rm my-react-app-1 || true
+docker rm my-react-app || true
 # docker rm my-react-app-2 || true
 docker rmi $(docker images -q) || true
